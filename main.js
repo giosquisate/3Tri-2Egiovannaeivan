de302ee9abb9c71324d29858c183c35034d86fc5
ivanlima1801 <160138777+ivanlima1801@users.noreply.github.com>
2024-10-29 08:52:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function criaCartao( categoria,pergunta,resposta){
-   let container = document.getElementById('container')
-   let article = document.createElement(article)
+function criaCartao(categoria,pergunta,resposta){
+   let container = document.getElementById('conteiner')
+   let article = document.createElement('article')
    article.className  = 'cartao'
-   article.innerHTML = 'Isso é um teste'
+   article.innerHTML = `
+   <div class="cartao-conteudo">
+      <h3> ${categoria} </h3>
+      <div class="cartao-conteudo-pergunta">
+         <p> ${pergunta} </p>
+      </div>
+      <div class="cartao-conteudo-resposta">
+         <p>${resposta} </p>
+      </div>
+   </div>
+
+   `
    container.appendChild(article)
 }
-
 criaCartao( 'Biologia', 'Qual a função da mitocondria', 'Produção de energia')
